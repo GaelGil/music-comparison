4e44fff73bae6739c4b767dfa4f01a5aabb9afa6
GaelGil <gaelgilbello27@gmail.com>
2022-04-25 15:59:08 -0700

    fixed the wav converter

--- random/mp_to_wave.py
+++ random/mp_to_wave.py
@@ -1,18 +1,38 @@
 import os
 from pydub import AudioSegment
+# from ..audio_data
 
-path = '../audio_data/mp3_data'
+# path to the mp3 audio data
+path = '../audio_data/test_mp3/'
 
+# directory
 directory = os.fsencode(path)
-    
+
+
+
+# loop through all the data in the directort
 for file in os.listdir(directory):
-     filename = os.fsdecode(file)
-     if filename.endswith(".mp3"): 
-        #  print(os.path.join(directory, filename))
-        # print(filename)
-        src = f'{path}/{filename}'
-        filename = filename[:-4]
-        dst = f'../audio_data/wav_data/{filename}.wav'
-        print(src)
-        sound = AudioSegment.from_mp3(src)
-        sound.export(dst, format='wav')
+   filename = os.fsdecode(file)
+   if filename.endswith(".mp3"): 
+      # the mp3 file
+      src = f'{path}/{filename}'
+      # remove .mp3 and add .wav
+      filename = f'{filename[:-4]}.wav'
+      # the destination of where we want to save our wav data to
+      dst = f'../audio_data/test_wav/{filename}'
+      # create a file where we can save that wav data to
+      f = open(dst, 'x') 
+      f.close()
+      # load the mp3
+      sound = AudioSegment.from_mp3(src)
+      # convert to wav and save to a directory
+      sound.export(dst, format='wav')
+
+
+
+def mp3_to_wav(origin:str, destination:str):
+   """
+   Function to convert all mp3 files from a directory to wav into another
+   directory. 
+   """
+   return
